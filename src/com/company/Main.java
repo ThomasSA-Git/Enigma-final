031671a497549d54d6871246c2804fb59223f3d8
ThomasSA-Git <thom45w9@stud.kea.dk>
2022-03-02 21:37:58 +0100

    Der mangler en løsning på for-loop i for-loop i charToNum-metoden. Ny løsning på metoder.

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,47 +1,66 @@
 package com.company;
 
 import java.lang.reflect.Array;
+import java.util.Arrays;
 import java.util.Scanner;
 
 public class Main {
 
-  char[] alphabet = {'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z', 'Æ', 'Ø', 'Å'};
-  int shiftValue = 2;
-  String text = " ";
+  char[] alphabet = {' ', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z', 'Æ', 'Ø', 'Å'};
+  int shiftValue = 1;
+  Scanner sc = new Scanner(System.in);
 
   public void runProgram() {
-
-  }
-
-  public void typeText() {
-    Scanner sc = new Scanner(System.in);
-    System.out.println("Indtast teksten du ønsker at kryptere/afkryptere");
-    text = sc.next();
-    stringToChar(text);
-  }
-
-  public void stringToChar(String text) {
-    char[] ch = new char[text.length()];
-    for (int i = 0; i < text.length(); i++) {
-      ch[i] = text.charAt(i);
+    System.out.println("Velkommen til Enigma. Her kan du kryptere eller dekryptere en tekst ved hjælp af Caesar Cipher");
+    System.out.println("Ønsker du at:\n" +
+        "1. Kryptere\n" +
+        "2. Dekryptere");
+    int choice = sc.nextInt();
+    if (choice == 1) {
+      cryptCharToString(numToChar(cryptNewNumValue(charToNum(stringToChar(typeText())))));
     }
-    charToNum(ch);
+    if (choice == 2) {
+      decryptCharToString(numToChar(decryptNewNumValue(charToNum(stringToChar(typeText())))));
+    } else {
+      System.out.println("Ikke et gyldigt valg. Programmet genstartes.");
+      runProgram();
+    }
   }
 
-  public void charToNum(char[] array) {
-    for (int i = 0; i < array.length; i++) {
+  public String typeText() {
+    System.out.println("Indtast teksten herunder:");
+    String text = sc.next();
+    return text;
+  }
+
+  public char[] stringToChar(String text) {
+    char[] chArray = new char[text.length()];
+    for (int i = 0; i < text.length(); i++) {
+      chArray[i] = text.charAt(i);
+    }
+
+    return chArray;
+  }
+
+  public int[] charToNum(char[] charArray) {
+
+    int[] valueArray = new int[charArray.length];
+    for (int i = 0; i < charArray.length; i++) {
+      char test = charArray[i];
       for (int o = 0; o < alphabet.length; o++) {
-        if (alphabet[o] == array[i]) {
-          int[] valueArray = new int[text.length()];
+        char test2 = alphabet[5];
+
+        if (test == test2) {        //Der er en fejl i loopet, muligvis ved if der gør at valueArray ikke får tilføjet den rette index
+          System.out.println(test2);
           valueArray[i] = o;
-          newNumValue(valueArray);
         }
       }
     }
 
+    return valueArray;
   }
 
-  public void newNumValue(int[] newValueArray) {
+  public int[] cryptNewNumValue(int[] newValueArray) {
     for (int i = 0; i < newValueArray.length; i++) {
       newValueArray[i] += shiftValue;
       if (newValueArray[i] > newValueArray.length) {
@@ -49,51 +68,48 @@ public class Main {
       }
 
     }
-    numToChar(newValueArray);
+    return newValueArray;
   }
 
-  public void numToChar(int[] newValue) {
-  for (int i = 0; i < newValue.length; i++){
-    for (int o = 0; o < alphabet.length; i++){
-      if(alphabet[o] == newValue[i]){
-        char[] newChar = new char[newValue.length];
-        newChar[i] = alphabet[o];
+  public int[] decryptNewNumValue(int[] newValueArray) {
+    for (int i = 0; i < newValueArray.length; i++) {
+      newValueArray[i] += shiftValue;
+      if (newValueArray[i] > newValueArray.length) {
+        newValueArray[i] -= newValueArray.length;
+      }
+
+    }
+    return newValueArray;
+  }
+
+  public char[] numToChar(int[] newValue) {
+    char[] newChar = new char[newValue.length];
+    for (int i = 0; i < newValue.length; i++) {
+      for (int o = 0; o < alphabet.length; i++) {
+        if (alphabet[o] == newValue[i]) {
+
+          newChar[i] = alphabet[o];
+        }
       }
     }
+    return newChar;
   }
 
-}
 
-  /*
-  public void charToString() {
-
-
+  public void cryptCharToString(char[] arrayToString) {
+    String cryptedText = String.valueOf(arrayToString);
+    System.out.println("Her er resultatet af krypteringen:\n" + cryptedText);
   }
 
-  /*
-Metoder:
-Run program.
-Choose cypher type.
-Globalt array med alfabetet? Evt. find talværdien for hver bogstav i alfabetet.
-Hiv de enkelte bogstafver ud af string.
-Shiftværdi ændrer dem?
-Opbyg ny string med dem en af gangen (string-builder).
+  public void decryptCharToString(char[] arrayToString) {
+    String cryptedText = String.valueOf(arrayToString);
+    System.out.println("Her er resultatet af dekrypteringen:\n" + cryptedText);
+  }
 
-Dekryptering - modsat af kryptering.
-
-
-Brug unicode til displacement (shift)
-
-
-*/
   public static void main(String[] args) {
     Main obj = new Main();
 
-    obj.stringToChar();
-    obj.charToNum();
-    //obj.newNumValue();
-    //obj.numToChar();
-    //System.out.println(obj.charToString());
+    obj.runProgram();
 
   }
 }
